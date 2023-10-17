a09cfe2f123e7ca14cb5197df13281056343f453
Anthony Derose <anthony.derose@acensi.fr>
2023-10-17 13:52:31 +0200

    datas

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,8 +1,20 @@
 export default [
 	{
-		title: "Irc: Develop an IRC server in C++",
-		imgUrl: "/assets/irssi.png", 
-		stack: ["C++98"],
+		title: "Acenstream",
+		imgUrl: "https://via.placeholder.com/150",
+		stack: ["React", "Node", "Fastify", "Sequelize", "Socket.io", "Mui", "Electron.Js", "pgAdmin4", "PostgreSQL"],
+		link: "",
+	},
+	{
+		title: "Syneryx",
+		imgUrl: "https://via.placeholder.com/150",
+		stack: ["React", "Node", "Fastify", "Sequelize", "Socket.io", "Mui", "pgAdmin4", "PostgreSQL", "Redis"],
+		link: "",
+	},
+	{
+		title: "Transcendence",
+		imgUrl: "/assets/Transcendence-project.jpeg", 
+		stack: ["React", "Typescript", "Nest", "Express", "PostgreSQL", "TypeORM", "Websockets", "API REST"],
 		link: "",
 	},
 	{
@@ -12,9 +24,15 @@ export default [
 		link: "",
 	},
 	{
-		title: "Transcendence",
-		imgUrl: "/assets/Transcendence-project.jpeg", 
-		stack: ["React", "Typescript", "Nest (Express)", "PostgreSQL", "TypeORM", "Websockets", "API REST"],
+		title: "Irc: Develop an IRC server in C++",
+		imgUrl: "/assets/irssi.png", 
+		stack: ["C++98"],
+		link: "",
+	},
+	{
+		title: "Containers: Avl Tree",
+		imgUrl: "https://via.placeholder.com/150", 
+		stack: ["C++98"],
 		link: "",
 	},
 	{
--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -1,26 +1,20 @@
 export default [
 	{
-		year: 'since 2023',
-		title: "Développeur",
-		duration: "still",
-		details: 'Développeur full-stack.'
-	},
-	{
 		year: '2023',
 		title: "Développeur",
-		duration: "6 months",
-		details: 'Stage chez Acensi en tant que développeur full-stack.'
+		duration: "6 mois de stage + CDI",
+		details: 'Développeur full-stack chez l\'ESN Acensi.'
 	},
 	{
 		year: '2020',
 		title: "Architecte en Technologie du Numérique (Formation)",
-		duration: "3 years",
+		duration: "3 ans",
 		details: 'Architecte en Technologie du Numérique à l\'école 42.'
 	},
 	{
 		year: '2016',
 		title: "Gestionnaire de Base de Données",
-		duration: "4 years",
+		duration: "4 ans",
 		details: "Analyse de la complétude d'un dossier cartographique. \
 							Mise à jour de plans 2D comportant les schémas électriques unifilaires sur la zone de travaux (logiciel de CAO MicroStation, équivalent d'AutoCAD). \
 							Requêtes sur une BDD interne à l'entreprise."
