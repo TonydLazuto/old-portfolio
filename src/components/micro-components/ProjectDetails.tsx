41d42f2b72af409c30f713a1ef90092ffc0dd037
Tony D <2785518+TonydLazuto@users.noreply.github.com>
2023-11-08 08:12:06 +0100

    undo projectDetails + fix project hover bug

--- src/components/micro-components/ProjectDetails.tsx
+++ src/components/micro-components/ProjectDetails.tsx
@@ -1,5 +1,4 @@
-import { Box, Button, Modal } from "@mui/material"
-import CloseIcon from '@mui/icons-material/Close';
+import { Box } from "@mui/material"
 
 interface Project {
 	key: string
@@ -11,44 +10,29 @@ interface Project {
 }
 
 interface Props {
-    open: boolean
-    handleOpen: (bool: boolean) => void
     project: Project
 }
 
 const ProjectDetails = (props: Props) => {
-    const { open, handleOpen, project } = props;
+    const { project } = props;
     const { title, description, imgUrl, stack, details } = project;
-    const handleClose = () => {
-        handleOpen(false);
-	};
   return (
     <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="child-modal-title"
-        aria-describedby="child-modal-description"
-    >
-        <Box
-         className='
+      <Box
+        className="
             flex flex-col items-center justify-center
             absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
-            w-1/2 h-1/2'
-        >
-            <h2 id="child-modal-title">{title}</h2>
-            <p id="child-modal-description">
-                {details.map((detail, index) => (
-                    <li key={index}>{detail}</li>
-                ))}
-            </p>
-            <Button onClick={handleClose}>
-                <CloseIcon />
-            </Button>
-        </Box>
-        </Modal>
+            w-1/2 h-1/2"
+      >
+        <h2 id="title">{title}</h2>
+        <p id="description">
+          {details.map((detail, index) => (
+            <li key={index}>{detail}</li>
+          ))}
+        </p>
+      </Box>
     </div>
-  )
+  );
 }
 
 export default ProjectDetails
--- src/components/micro-components/ProjectItem.tsx
+++ src/components/micro-components/ProjectItem.tsx
@@ -16,10 +16,6 @@ const ProjectItem = (project: Project) => {
 	const [hidden, setHidden] = useState('hidden');
 	const [projectItemClass, setProjectItemClass] = useState('');
 	const [imgClass, setImgClass] = useState('');
-	const [open, setOpen] = useState(false);
-	const handleOpen = (bool: boolean) => {
-		setOpen(bool);
-	};
 
     const obsRef = useObserver();
 
@@ -27,7 +23,7 @@ const ProjectItem = (project: Project) => {
 		e.preventDefault();
 		if (hidden) {
 			setHidden('');
-			setProjectItemClass('cursor-pointer animate-projectHoverEnter');
+			setProjectItemClass('animate-projectHoverEnter');
 			setImgClass('animate-projectImgHoverEnter');
 		}
 	}
@@ -40,49 +36,59 @@ const ProjectItem = (project: Project) => {
 		}
 	}
 
+	// const handleProjectDetails = (e : React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+	// 	e.preventDefault();
+	// }
+
 	return (
-		<Box ref={obsRef}
-			className={`rounded-md overflow drop-shadow-3xl
+    <Box
+      ref={obsRef}
+      className={`rounded-md overflow drop-shadow-3xl
 				w-96 mb-4 ${projectItemClass}`}
-		>
-			<Box onClick={() => handleOpen(true)}
-				onMouseEnter={handleProjectHoverEnter}
-				onMouseLeave={handleProjectHoverLeave}
-			>
-				<img
-					src={imgUrl}
-					alt="project"
-					className={`w-full h-72 rounded-t-lg
+      onMouseEnter={handleProjectHoverEnter}
+      onMouseLeave={handleProjectHoverLeave}
+    >
+      <Box
+      // onClick={handleProjectDetails}
+      >
+        <img
+          src={imgUrl}
+          alt="project"
+          className={`w-full h-72 rounded-t-lg
 					object-cover ${imgClass}`}
-				/>
-				<Box className='p-2'>
-					<h3 className='text-lg md:text-xl
-							mb-2 md:mb-3 font-semibold'>
-						{title}
-					</h3>
-					<h4 className={`text-lg md:text-lg
+        />
+        <Box className="p-2">
+          <h3
+            className="text-lg md:text-xl
+							mb-2 md:mb-3 font-semibold"
+          >
+            {title}
+          </h3>
+          <h4
+            className={`text-lg md:text-lg
 							mb-2 md:mb-3
-							animate-projectDescriptionPop ${hidden}`}>
-						{description}
-					</h4>
-					<Box className='flex flex-wrap gap-2
-							flex-row items-center justify-start text-xs md:text-sm'>
-						{stack.map((item) => (
-							<span key={item} className='inline-block px-2 py-1
-									font-semi-bold border-2 border-stone-900 rounded-md'>
-								{item}
-							</span>
-						))}
-					</Box>
-				</Box>
-			</Box>
-			<ProjectDetails
-				open={open}
-				handleOpen={handleOpen}
-				project={project}
-			/>
-		</Box>
-	)
+							animate-projectDescriptionPop ${hidden}`}
+          >
+            {description}
+          </h4>
+          <Box
+            className="flex flex-wrap gap-2
+							flex-row items-center justify-start text-xs md:text-sm"
+          >
+            {stack.map((item) => (
+              <span
+                key={item}
+                className="inline-block px-2 py-1
+									font-semi-bold border-2 border-stone-900 rounded-md"
+              >
+                {item}
+              </span>
+            ))}
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
 export default ProjectItem
