d99f91634258b8507817c021f31e0c0e56247378
Tony D <2785518+TonydLazuto@users.noreply.github.com>
2023-12-27 00:11:51 +0100

    SEO: modify syntax robots.txt

--- src/components/micro-components/SocialsMedia.tsx
+++ src/components/micro-components/SocialsMedia.tsx
@@ -27,48 +27,48 @@ const SocialsMedia = ({ isMobile }: IsMobile) => {
       className={`flex justify-around items-center
         w-24 h-12 ${animateHeaderDelay3}`}
     >
-      <a
-        href="https://github.com/TonydLazuto"
-        className="block w-full h-full
+      <Tooltip
+        onMouseEnter={handleStackHoverEnter}
+        onMouseLeave={handleStackHoverLeave}
+        title="GitHub"
+        placement="left"
+        TransitionComponent={Fade}
+        TransitionProps={{ timeout: 400 }}
+        arrow
+      >
+        <a
+          href="https://github.com/TonydLazuto"
+          className="block w-full h-full
           flex items-center justify-center
           text-gray-100
           hover:text-gray-400
           hover:border-b-8 hover:border-sky-600
           transition-all duration-300 ease-out"
-      >
-        <Tooltip
-          onMouseEnter={handleStackHoverEnter}
-          onMouseLeave={handleStackHoverLeave}
-          title="GitHub"
-          placement="left"
-          TransitionComponent={Fade}
-          TransitionProps={{ timeout: 400 }}
-          arrow
         >
           <GitHubIcon fontSize="large" />
-        </Tooltip>
-      </a>
-      <a
-        href="https://www.linkedin.com/in/anthony-d%C3%A9ros%C3%A9-957110124/"
-        className="block w-full h-full
+        </a>
+      </Tooltip>
+      <Tooltip
+        onMouseEnter={handleStackHoverEnter}
+        onMouseLeave={handleStackHoverLeave}
+        title="LinkedIn"
+        placement="right"
+        TransitionComponent={Fade}
+        TransitionProps={{ timeout: 400 }}
+        arrow
+      >
+        <a
+          href="https://www.linkedin.com/in/anthony-d%C3%A9ros%C3%A9-957110124/"
+          className="block w-full h-full
           flex items-center justify-center
           text-gray-100
           hover:text-gray-400
           hover:border-b-8 hover:border-sky-600
           transition-all duration-300 ease-out"
-      >
-        <Tooltip
-          onMouseEnter={handleStackHoverEnter}
-          onMouseLeave={handleStackHoverLeave}
-          title="LinkedIn"
-          placement="right"
-          TransitionComponent={Fade}
-          TransitionProps={{ timeout: 400 }}
-          arrow
         >
           <LinkedInIcon fontSize="large" />
-        </Tooltip>
-      </a>
+        </a>
+      </Tooltip>
     </div>
   );
 }
