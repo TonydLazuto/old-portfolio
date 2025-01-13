e346eb509c2349e53255859273d0886be0a0e44e
Anthony <2785518+TonydLazuto@users.noreply.github.com>
2025-01-13 21:54:52 +0100

    getYear() in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,11 @@
 const Footer = () => {
+	const year = new Date().getFullYear()
 	return (
 		<div
 			className='h-20 bg-gradient-to-t from-shade0 via-slate-800 to-indigo-900
 				flex justify-center items-center
 				font-bold text-white'>
-				<p>Copyright © Anthony Derose - 2024</p>
+				<p>Copyright © Anthony Derose - {year}</p>
 		</div>
 	)
 }
